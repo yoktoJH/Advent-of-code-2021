d60cd9520ba6e0a5b0dd22f3f1a0950804988023
yokto <jozikh@hotmail.com>
2021-12-20 02:15:09 +0100

    ciastka dna 18 pice mi ti crashlo

---
+++ den18/den18.cpp
@@ -0,0 +1,16 @@
+for (size_t i = 0; i < 99; i++)
+{
+    scitanie();
+    int zmena = 1;
+    while (zmena == 1)
+    {
+        zmena = explooosion();
+        znaky.shrink_to_fit();
+        if (zmena == 0)
+        {
+            zmena = split();
+
+        }
+    }
+
+}
