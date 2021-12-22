c6e8b9deaf48fb115867330c76ecf2a832a3742e
yokto <jozikh@hotmail.com>
2021-12-22 02:05:21 +0100

    prva polovica dna 20

--- den20/den20.cpp
+++ den20/den20.cpp
@@ -4,8 +4,9 @@
 #include <string>
 #include <algorithm>
 std::vector<std::string> obrazok;
+std::vector<std::string> obrazok2;
 int main()
-{
+{   
     std::string vzor = "#####.##....##....##.#.....##....##..########.#.##.#.#.##.#..##.#.####.######.#####.######.##..#######.#.#...#..#.####..####...#.####..#......#...#...##.#.....#....#..###.#..##....#.#....#...##.###.#.#..##.......####.........#.#.###.#.#.....#..##..##.#.##..###.##.###.#....#.#..##.#.......###..#.#.#.#.#.....#..#.###.##..##...#....##...##.##...##.#..####.#...#.####...####..#####.#####.#.##...#.###.#######.###..#..##.#.#..#.#.#######.#####.#.##.#.#...##.######.#...##.##.#.........##...##.....#.###.#.##.#.####.";
     std::string vypln = "";
     for (size_t i = 0; i < 106; i++)
@@ -22,12 +23,86 @@ int main()
     }
     obrazok.push_back(vypln); obrazok.push_back(vypln); obrazok.push_back(vypln);
     
-    
-    
-    for (int i = 0; i <106; i++)
-    {
-        std::cout << obrazok[i]<<std::endl;
+    for (size_t i = 0; i < 106; i++)
+    {   
+        std::string riadok = "";
+        for (size_t j = 0; j < 106; j++)
+        {
+            std::string bod;
+            if (((i == 0) || (i == 105)) || ((j == 0)||(j == 105))) {
+                riadok += "#";
+            }
+            else
+            {
+                for (int l = -1; l <=1; l++)
+                {
+                    for (int k = -1; k <=1; k++)
+                    {   
+                        if (obrazok[i + l][j + k] == '#')
+                        {
+                            bod+="1";
+                        }
+                        else
+                        {
+                            bod += "0";
+                        }
+                        
+                    }
+                }
+                int b;
+                b = stoi(bod, 0, 2);
+                riadok += vzor[b];
+            }
+        }
+        obrazok2.push_back(riadok);
     }
-    std::cout << vypln;
+    obrazok = obrazok2;
+    obrazok2.clear();
+    for (size_t i = 0; i < 106; i++)
+    {
+        std::string riadok = "";
+        for (size_t j = 0; j < 106; j++)
+        {
+            std::string bod;
+            if (((i == 0) || (i == 105)) || ((j == 0) || (j == 105))) {
+                riadok += ".";
+            }
+            else
+            {
+                for (int l = -1; l <= 1; l++)
+                {
+                    for (int k = -1; k <= 1; k++)
+                    {
+                        if (obrazok[i + l][j + k] == '#')
+                        {
+                            bod += "1";
+                        }
+                        else
+                        {
+                            bod += "0";
+                        }
+                    }
+                }
+                int b;
+                b = stoi(bod, 0, 2);
+                riadok += vzor[b];
+            }
+        }
+        obrazok2.push_back(riadok);
+    }
+    int p=0;
+    for (size_t i = 0; i < 106; i++)
+    {
+
+        for (size_t j = 0; j < 106; j++)
+        {
+            if (obrazok2[i][j] == '#')
+            {
+                p += 1;
+            }
+        }
+    }
+
+    std::cout << p;
 }
 
